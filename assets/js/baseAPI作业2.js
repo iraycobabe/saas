5bddc12ca7658032de71fbe0913586259fddf3b8
“hejiawei” <469994761@qq.com>
2022-12-06 13:17:39 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/baseAPI作业2.js
@@ -0,0 +1,9 @@
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function(options){
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    //统一为有权限的接口设置 headers 请求头
+    //indexOf检索字符串 如果没有包含 结果返回-1
+})
